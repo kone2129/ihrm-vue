5761d0ab0446faa07c64a94eba577b95c95437f1
kone <kone.wang@mic.com.tw>
2020-01-19 16:50:05 +0800

    公司前后端联调

--- project-saas-hrm-vue-master/src/mock/index.js
+++ project-saas-hrm-vue-master/src/mock/index.js
@@ -12,5 +12,5 @@ Mock.mock(/\/frame\/profile/, 'post', ProfileAPI.profile)
 Mock.mock(/\/frame\/login/, 'post', LoginAPI.login)
 //配置模拟数据接口
 
-Mock.mock(/\/company\/+/, 'get', CompanyAPI.sassDetail)//根据id查询
-Mock.mock(/\/company/, 'get', CompanyAPI.list) //访问企业列表
+// Mock.mock(/\/company\/+/, 'get', CompanyAPI.sassDetail)//根据id查询
+// Mock.mock(/\/company/, 'get', CompanyAPI.list) //访问企业列表
